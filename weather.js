a6a8c7ecd005d792174de1ece91213452b854484
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-06-28 17:36:38 +0600

    weather name

--- weather.js
+++ weather.js
@@ -5,9 +5,10 @@ document.getElementById('submit-btn').addEventListener('click', function () {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=805af68c71f238865fc5e9b2614a0e87`)
         .then(res => res.json())
         .then(data => {
-           
+           var weatherDetails = data.weather[0];
             document.getElementById('city-name').innerText = data.name;
             document.getElementById('city-temp').innerText = data.main.temp;
+            document.getElementById('city-weather').innerText = weatherDetails.main;
             
            
 
